169923630a23318c22267f7b64cfb017938b6556
AntonioMoreno <antoniohdzmoreno@hotmial.com>
2022-06-07 23:29:54 -0600

    Rules page base structure creation

--- src/component/pages/home.js
+++ src/component/pages/home.js
@@ -8,8 +8,8 @@ export default function Home() {
   return (
     <>
     <div id="Tabla">
+    <div id="hometabla">
     <div>
-
     <div id="Red">
     <img id="cora" src={CT} alt="Twitter"></img>
     <img id="cora" src={CT} alt="Twitter"></img>
@@ -32,6 +32,7 @@ export default function Home() {
     <img id="cora2"src={HI} alt=""></img></h3>
     <h3 id="Status">I like</h3>
     
+    </div>
     </div>
     </div>
     </div>
---
+++ src/component/pages/rules.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Header from '../header'
+
+
+function Rules() {
+  return (
+    <>
+    <Header></Header>
+    <div id="Tabla">
+    <ul id="lista">
+        <li><b class = "negrita">I don't draw</b>nsfw,gore,mecas.</li>
+        <li>Payment is by <b class = "negrita">PayPal.</b></li>
+        <li>Prices in <b class = "negrita">US Dollars.</b></li>
+        <li><b class = "negrita">Your draw can have corrections, just dm on time.</b></li>
+        <li><b class = "negrita">Commercial use? dm me.</b></li>
+    </ul>
+    </div>
+    </>
+  )
+}
+
+export default rules
